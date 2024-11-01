90ed66c2ed8a841ffd62e598eb7203fe432bee16
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2024-11-01 12:34:20 +0200

    1

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
       url: 'https://www.google.com.eg/maps/place/Fresh+Bar+-+%D9%81%D8%B1%D9%8A%D8%B4+%D8%A8%D8%A7%D8%B1%E2%80%AD/@29.9795032,31.3232923,17.26z/data=!4m7!3m6!1s0x1458391036f77857:0x909f99ab38b896ad!8m2!3d29.9795287!4d31.3241247!15sCg_Zgdix2YrYtCDYqNin2LGSARBzb2Z0X2RyaW5rc19zaG9w4AEA!16s%2Fg%2F11sv0z662s?entry=tts&g_ep=EgoyMDI0MTAyOS4wIPu8ASoASAFQAw%3D%3D'
     },
     {
-      text: 'Number 1',
+      text: 'Phone Number 1',
       url: 'tel:01122611810',
       icon: 'yes'
     },
     {
-      text: 'Number 2',
+      text: 'Phone Number 2',
       url: 'tel:01001270203',
       icon: 'yes'
     }
@@ -32,7 +32,7 @@ function App() {
         {data.map((item, index) => {
           return (
             <Stack sx={{ width: { xs: '90%', sm: '70%', md: '60%' } }} key={index}>
-              <a style={{ textDecoration: 'none', backgroundColor: 'white', padding: '20px', display: 'flex', borderRadius: '15px', textAlign: 'center', fontWeight: 'bold', fontSize: '40px', color: '#009459', justifyContent: 'center', alignItems: 'center' }} href={item.url} >
+              <a style={{ textDecoration: 'none', backgroundColor: 'white', padding: '20px', display: 'flex', borderRadius: '15px', textAlign: 'center', fontWeight: 'bold', fontSize: '30px', color: '#009459', justifyContent: 'center', alignItems: 'center' }} href={item.url} >
                 {item.icon &&
                   <Call style={{ fontSize: '40px' }} />
                 }  {item.text}
